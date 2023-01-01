7e8981a2d60653e7482d0cec355a18f9db1c0708
Tanisha Choudhary <94930302+tanishach03@users.noreply.github.com>
2023-01-01 22:48:23 +0530

    level 2 auth commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose= require("mongoose");
+const encrypt = require("mongoose-encryption");
 const ejs = require("ejs");
 
 const app = express();
@@ -14,10 +15,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://admin-tanisha:Tanisha@cluster0.nha7vaw.mongodb.net/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String, 
     password: String
-}
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
