80d92015db5e8e4ff74f2f301e413a8ee069c270
Tanisha Choudhary <94930302+tanishach03@users.noreply.github.com>
2023-01-01 20:41:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esver
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose= require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://admin-tanisha:Tanisha@cluster0.nha7vaw.mongodb.net/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String, 
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+            if (foundUser.password=== password){
+                res.render("secrets");
+            }
+        }}
+    });
+})
+
+
+
+app.listen("3000", function(){
+    console.log("Server started on port 3000");
+})
+
+
